cca6e8334674800fa81afb44a6a724a6c714d7df
Shobhit Behl <shobhitbehl98@gmail.com>
2021-09-11 15:52:48 +0530

    minrem

---
+++ Minimum Remove To Make Valid Parentheses.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static String reverseParentheses(String s) {
+        Stack<Integer> st=new Stack<>();
+        char[] arr=s.toCharArray();
+        for(int i=0;i<arr.length;i++){
+           if(arr[i]=='('){
+               st.push(i);
+           }else if(arr[i]==')'){
+               if(st.size()>0){
+                   st.pop();
+               }else{
+                   arr[i]='.';
+               }
+           }
+        }
+        while(st.size()>0){
+            int idx=st.pop();
+            arr[idx]='.';
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!='.'){
+            sb.append(arr[i]);
+            }
+        }
+        
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        String result = reverseParentheses(read.readLine());
+        System.out.println(result);
+        
+    }
+}
