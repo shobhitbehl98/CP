f0bcf3908e444207f50b670e7cc536100e201681
Shobhit Behl <shobhitbehl98@gmail.com>
2021-09-28 20:22:56 +0530

    update on 1574A

--- A_Regular_Bracket_Sequences.java
+++ A_Regular_Bracket_Sequences.java
@@ -1,3 +1,23 @@
+/*
+if n=4
+then output would be 
+(((()))) --> 0 4
+()((())) --> 1 3
+(())(()) --> 2 2
+((()))() --> 3 1
+
+as we can observe brackets on the left increase from 0 1 2 3 and brackets on the right decrease from 4 3 2 1
+so we will have to use a for loop which will iterate from 0 to 3 -- int i=0;i<4;i++
+
+and inside that for loop we will have to use another for loop for left brackets'(' and second for ')' left brackets int x=0;x<i(i will go from 0 to 2 as loop i will go from 0 to 3 remember that).
+
+the third loop will be used for from i=0;i<n-i; for'(' and fourth will run i=0;i<n-i; for ')'
+
+dry run the solution to get complete clarity.
+
+Time Complexity : O(N^2);  
+
+*/
 import java.io.*;
 import java.util.*;
 public class A_Regular_Bracket_Sequences {
