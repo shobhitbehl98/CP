56d8adc70242e1f6fbdf7d3d94e110890c55435b
Shobhit Behl <shobhitbehl98@gmail.com>
2021-12-14 17:15:21 +0530

    done

--- C_Minimize_Distance.java
+++ C_Minimize_Distance.java
@@ -1,73 +1,110 @@
 import java.io.*;
 import java.util.*;
+
 public class C_Minimize_Distance {
 
-    static class FastReader
-    {
+    static class FastReader {
         BufferedReader br;
         StringTokenizer st;
- 
-        public FastReader()
-        {
-            br = new BufferedReader(new
-                    InputStreamReader(System.in));
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
         }
- 
-        String next()
-        {
-            while (st == null || !st.hasMoreElements())
-            {
-                try
-                {
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
                     st = new StringTokenizer(br.readLine());
-                }
-                catch (IOException  e)
-                {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
             return st.nextToken();
         }
-        int nextInt()
-        {
+
+        int nextInt() {
             return Integer.parseInt(next());
         }
- 
-        long nextLong()
-        {
+
+        long nextLong() {
             return Long.parseLong(next());
         }
- 
-        double nextDouble()
-        {
+
+        double nextDouble() {
             return Double.parseDouble(next());
         }
-        String nextLine()
-        {
+
+        String nextLine() {
             String str = "";
-            try
-            {
+            try {
                 str = br.readLine();
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
             return str;
         }
     }
-    
-    public static void main(String[] args){
-         FastReader f=new FastReader();
-         StringBuilder sb=new StringBuilder();
-         int t = f.nextInt();
-         while(t-->0){
-            String[] s=f.nextLine().split(" ");
-                 
-         }
+
+    public static void main(String[] args) {
+        FastReader f = new FastReader();
+        StringBuilder sb = new StringBuilder();
+        int t = f.nextInt();
+        while (t-- > 0) {
+            int n = f.nextInt();
+            int k = f.nextInt();
+            ArrayList<Integer> a = new ArrayList<>();
+            ArrayList<Integer> b = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                int h = f.nextInt();
+                if (h < 0) {
+                    h *= -1;
+                    b.add(h);
+                } else {
+                    a.add(h);
+                }
+            }
+            Collections.sort(a);
+            Collections.sort(b);
+            long ans = 0;
+            if (b.size()==0||(a.size()>0&&(a.get(a.size() - 1) > b.get(b.size() - 1)))) {
+                int i = b.size() - 1;
+                while (i >= 0) {
+                    ans += 2 * (b.get(i));
+                    i -= k;
+                }
+                boolean y = false;
+                int j = a.size() - 1;
+                while (j >= 0) {
+                    if (y) {
+                        ans += 2 * (a.get(j));
+                    } else {
+                        ans += a.get(j);
+                        y = true;
+                    }
+                    j -= k;
+                }
+            } else {
+                int j = a.size() - 1;
+                while (j >= 0) {
+                    ans += 2*(a.get(j));
+                    j -= k;
+                }
+                boolean y = false;
+                int i = b.size() - 1;
+                while (i >= 0) {
+                    if (y) {
+                        ans += 2 * (b.get(i));
+                    } else {
+                        ans += b.get(i);
+                        y = true;
+                    }
+                    i -= k;
+                }
+
+            }
+            sb.append(ans+"\n");
+
+        }
+        System.out.println(sb);
     }
-    }
-
-
-    
-
+}
